0ae1d1c812580cc1a785033d5c87549ed549608f
fabianGerman <fabianontiveros_7@hotmail.com>
2022-11-06 02:06:15 -0300

    prueba

---
+++ Assets/Script/Ontiveros_German/NavMesh_controller.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMesh_controller : MonoBehaviour
+{
+    public Transform objetivo;
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = objetivo.position;
+        //NAVEGACION MEDIANTE CLICK
+        /*
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                agent.destination = hit.point;
+            }
+        }*/
+    }
+}
